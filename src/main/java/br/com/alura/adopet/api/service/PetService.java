4e13a2958a35feee77286b0b3e4551fcac475533
Lucas Mineiro <lucas0asm@outlook.com>
2024-05-31 16:27:41 -0300

    Desafio: refatorando pet

---
+++ src/main/java/br/com/alura/adopet/api/service/PetService.java
@@ -0,0 +1,22 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.pet.PetDto;
+import br.com.alura.adopet.api.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PetService {
+    @Autowired
+    private PetRepository petRepository;
+
+    public List<PetDto> petsDisponiveis() {
+        return petRepository
+                .findAllByAdotadoFalse()
+                .stream()
+                .map(PetDto::new)
+                .toList();
+    }
+}
