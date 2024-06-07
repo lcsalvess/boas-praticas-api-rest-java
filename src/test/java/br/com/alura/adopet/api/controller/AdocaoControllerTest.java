3617f11a2c7e28fe00910e1d2d6cceb18e5a233e
Lucas Mineiro <lucas0asm@outlook.com>
2024-06-06 21:03:38 -0300

    gerando JSON com JacksonTester

--- src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -1,12 +1,15 @@
 package br.com.alura.adopet.api.controller;
 
+import br.com.alura.adopet.api.dto.adocao.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.service.AdocaoService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -15,12 +18,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureJsonTesters
 class AdocaoControllerTest {
     @Autowired
     private MockMvc mvc;
 
     @MockBean
     private AdocaoService adocaoService;
+
+    @Autowired
+    private JacksonTester<SolicitacaoAdocaoDto> jsonDto;
 
     @Test
     @DisplayName("Código 400 para solicitação COM ERROS")
@@ -56,6 +63,22 @@ class AdocaoControllerTest {
         ).andReturn().getResponse();
         //ASSERT
         Assertions.assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    @DisplayName("Código 200 para solicitação SEM ERROS (OUTRA MANEIRA)")
+    void cenario03() throws Exception {
+        //ARRANGE
+        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(1L, 1L, "Motivo qualquer");
+        //ACT
+        var response = mvc.perform(
+                post("/adocoes")
+                        .content(jsonDto.write(dto).getJson())
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andReturn().getResponse();
+        //ASSERT
+        Assertions.assertEquals(200, response.getStatus());
+        Assertions.assertEquals("Adoção solicitada com sucesso!", response.getContentAsString());
     }
 
 }
